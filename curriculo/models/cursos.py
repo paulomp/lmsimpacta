2a6d3e74648aba893cfd757c4c9566d63f19b338
Paulo Miguel Patricio Santana <1802175@SALAS.AULAS>
2019-05-15 22:18:30 -0300

    Parte da aula

--- curriculo/models/cursos.py
+++ curriculo/models/cursos.py
@@ -1,8 +1,17 @@
 from django.db import models
+from curriculo.models.disciplinas import Disciplina
+
 
 class Curso(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     sigla = models.CharField(max_length=5, unique=True)
+    disciplinas = models.ManyToManyField(Disciplina, through='MatrizCurricular')
 
     def __str__(self):
         return self.nome
+
+class MatrizCurricular(models.Model):
+
+    semestre = models.IntegerField()
+    curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.SET_NULL, null=True)
--- curriculo/models/disciplinas.py
+++ curriculo/models/disciplinas.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.core.exceptions import ValidationError
 from contas.models import Coordenador
 
 class Disciplina(models.Model):
@@ -26,3 +26,11 @@ class Disciplina(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def clean(self):
+        if self.percentual_teorico > 100 or self.percentual_teorico < 0:
+            raise ValidationError('O percentual teórico deve estar entre 0 e 100.')
+        if self.percentual_pratico > 100 or self.percentual_pratico < 0:
+            raise ValidationError('O percentual prático  deve estar entre 0 e 100.')
+        if self.percentual_teorico + self.percentual_pratico != 100:
+            raise ValidationError('A soma das porcentagens deve ser igual a 100')
